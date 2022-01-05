669defc2dac7044a19e0d72d0547be2cd94ae1a3
jeffboxer <jefferson_civil@hotmail.com>
2022-01-05 14:58:32 +0000

    inserting codes

--- views/js/scrits.js
+++ views/js/scrits.js
@@ -1,7 +1,20 @@
-/*!
-* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function draw_table(){
+    $("#myDiv").empty();
+    $.getHTMLuncached = function(url){
+        return $.ajax({
+            url: url,
+            type: 'GET',
+            cache: false,
+            success: function(html){
+                $("#myDiv").append(html)
+            }
+        });
+
+    };
+        $.getHTMLuncached("/get/html")
+};
+
+$(document).ready(function(){
+
+    draw_table;
+})
