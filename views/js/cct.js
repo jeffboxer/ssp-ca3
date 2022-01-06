0f2b4ab0c13ec9c7e23e78dafa6a2d06e87d966b
jeffboxer <jefferson_civil@hotmail.com>
2022-01-06 23:39:43 +0000

    Fixing Everything I have lost

--- views/js/cct.js
+++ views/js/cct.js
@@ -1,63 +1,59 @@
-
-function edit_row(no)
+function draw_table()
 {
- document.getElementById("edit_button"+no).style.display="none";
- document.getElementById("save_button"+no).style.display="block";
-	
- var name=document.getElementById("name_row"+no);
- var id=document.getElementById("id_row"+no);
- var age=document.getElementById("age_row"+no);
-	
- var name_data=name.innerHTML;
- var id_data=id.innerHTML;
- var age_data=age.innerHTML;
-	
- name.innerHTML="<input type='text' id='name_text"+no+"' value='"+name_data+"'>";
- id.innerHTML="<input type='text' id='id_text"+no+"' value='"+id_data+"'>";
- age.innerHTML="<input type='text' id='age_text"+no+"' value='"+age_data+"'>";
-}
+	$("#results").empty();
+	$.getJSONuncached = function (url)
+	{
+		return $.ajax(
+		{
+			url: url,
+			type: 'GET',
+			cache: false,
+			success: function (html)
+			{
+				$("#results").append(html);
+				select_row();
+			}
+		});
+	};
+	$.getJSONuncached("/get/html")
+};
 
-function save_row(no)
+function select_row()
 {
-   
- var name_val=document.getElementById("name_text"+no).value;
- var id_val=document.getElementById("id_text"+no).value;
- var age_val=document.getElementById("age_text"+no).value;
+	$("#menuTable tbody tr[id]").click(function ()
+	{
+		$(".selected").removeClass("selected");
+		$(this).addClass("selected");
+		var section = $(this).prevAll("tr").children("td[colspan='3']").length - 1;
+		var entree = $(this).attr("id") - 1;
+		delete_row(section, entree);
+	})
+};
 
- document.getElementById("name_row"+no).innerHTML=name_val;
- document.getElementById("id_row"+no).innerHTML=id_val;
- document.getElementById("age_row"+no).innerHTML=age_val;
-
- document.getElementById("edit_button"+no).style.display="block";
- document.getElementById("save_button"+no).style.display="none";
-}
-
-
-function delete_row(no)
+function delete_row(sec, ent)
 {
- document.getElementById("row"+no+"").outerHTML="";
-}
-
-function add_row()
-{
-    if(document.getElementById("new_name").value=="" || document.getElementById("new_id").value=="" ||  document.getElementById("new_age").value=="")
-    {alert("You need to fill all the fields before adding a new row.");}
-    else{
- var new_name=document.getElementById("new_name").value;
- var new_id=document.getElementById("new_id").value;
- var new_age=document.getElementById("new_age").value;
-	
- var table=document.getElementById("data_table");
- var table_len=(table.rows.length)-1;
- var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='name_row"+table_len+"'>"+new_name+"</td><td id='id_row"+table_len+"'>"
- +new_id+"</td><td id='age_row"+table_len+"'>"+new_age+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
- document.getElementById("new_name").value="";
- document.getElementById("new_id").value="";
- document.getElementById("new_age").value="";
-}
-}
+	$("#delete").click(function ()
+	{
+		$.ajax(
+		{
+			url: "/post/delete",
+			type: "POST",
+			data:
+			{
+				section: sec,
+				entree: ent
+			},
+			cache: false,
+			success: setTimeout(draw_table, 1000)
+		})
+	})
+};
 
 $(document).ready(function ()
 {
 	draw_table();
 });
+
+function refresh(){
+    location.reload();
+}
